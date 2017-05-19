a8d6d7c48d71f23dacad2ae482e1b3488f0d3943
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-05-18 22:17:08 -0500

    Authenticator code

---
+++ Google.Authenticator/SetupCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Google.Authenticator
+{
+    public class SetupCode
+    {
+        public string Account { get; internal set; }
+        public string AccountSecretKey { get; internal set; }
+        public string ManualEntryKey { get; internal set; }
+        public string QrCodeSetupImageUrl { get; internal set; }
+    }
+}
